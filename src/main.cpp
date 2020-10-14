cce5a647f163b5959c804bffadc7f835ffd1d0ea
tffoe <tffoem@gmail.com>
2020-10-15 00:03:41 +0300

    travis configured

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <gtest/gtest.h>
 #include "matrix.h"
 
-
 int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    Matrix* matrix = create_matrix_from_file("../matrix.mat");
+    if (!matrix) {
+        free_matrix(matrix);
+        return 0;
+    }
 
-    // Matrix* matrix = create_matrix_from_file("../matrix.mata");
-    // if (!matrix) {
-    //     free_matrix(matrix);
-    //     return 0;
-    // }
+    Matrix** matrices = split_matrix(matrix);
 
-    // Matrix** matrices = split_matrix(matrix);
+    print_matrix(matrix);
 
-    // print_matrix(matrix);
+    print_matrix(matrices[0]);
+    print_matrix(matrices[1]);
+    print_matrix(matrices[2]);
 
-    // print_matrix(matrices[0]);
-    // print_matrix(matrices[1]);
-    // print_matrix(matrices[2]);
+    free_matrix(matrix);
+    free_matrix(matrices[0]);
+    free_matrix(matrices[1]);
+    free_matrix(matrices[2]);
+    free(matrices);
 
-    // free_matrix(matrix);
-    // free_matrix(matrices[0]);
-    // free_matrix(matrices[1]);
-    // free_matrix(matrices[2]);
-    // free(matrices);
-
-    // return 0;
+    return 0;
 }
